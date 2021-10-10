8676106aa5fa16b1c00873f64d208171e1ae383c
522917595@qq.com <chengjie1004>
2021-10-10 08:20:55 +0800

    添加用户工程

--- module_user/src/main/java/com/yxy/module_user/UserActivity.kt
+++ module_user/src/main/java/com/yxy/module_user/UserActivity.kt
@@ -1,26 +1,14 @@
-package com.yxy.component.ui.activity
+package com.yxy.module_user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import com.yxy.component.R
 
 //import com.yxy.lib_common.ToastUtils
 
 
-/**
- * 参考文章1：https://juejin.cn/post/7005483236117725215
- *
- *
- * 参考文章2：https://juejin.cn/post/7015882134795583519
- * 功能组件：lib_xxx
- * 业务组件：module_xxx
- *
- *
- *
- */
-class MainActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         var button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
-            textView.text = "点击后的值:app666"
+            textView.text = "点击后的值:user666"
 //            ToastUtils.show(this, "666")
         }
 
